b11d38fb9faf442c1a43cb16623fd956a9b7bce9
Harald Kurz <harald.kurz1988@gmail.com>
2024-01-25 17:08:51 +0100

    Initial Commit

---
+++ Calories/CaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesApp.swift
+//  Calories
+//
+//  Created by Harald Kurz on 25.01.24.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calories
+//
+//  Created by Harald Kurz on 25.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
